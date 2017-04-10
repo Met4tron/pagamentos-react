d5cf860c13d527ab9adc311463202e67f1339cc0
Met4tron <yuurig@gmail.com>
2017-04-09 21:16:56 -0300

    Configuração do build do webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,5 +4,46 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
 
 	entry: './src/index.jsx',
+	output: {
+		path: _dirname + '/public',
+		filename: './app.js'
+	},
+	devServer: {
+		port: 3000,
+		contentBase: './public'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+		alias: {
+			modules: __dirname + '/node_modules',
+			jquery: 'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+			bootstrap: 'node_modules/admin-lte/bootstrap/js/bootstrap.js'
+		}
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery'
+		}),
+		new ExtractTextPlugin('app.css')
+	],
+	module {
+		loaders: [{
+			test: /.js[x]?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query: {
+				presets: ['es2015', 'react']
+				plugins: ['transform-object-rest-spread']
+			}		
+		}, {
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+		}, {
+			test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+			loader: 'file'
+		}]
+	}
 	
 }
